2ff1c65b67236bd453310e0627f8d3ba839cc356
Fortune Etsu <engr.etsu@gmail.com>
2025-06-30 00:29:31 +0100

    made changes to error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,14 +3,19 @@ import { useRouteError, Link } from 'react-router-dom';
 const ErrorPage = () => {
   const error = useRouteError();
 
+  // Default error message if error is null or lacks properties
+  const errorMessage = error?.statusText || error?.message || 'An unexpected error occurred';
+
   return (
     <div className="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
-      <Link to="/" className="error-link">Go to Home</Link>
+      <Link to="/" className="error-link">
+        Go to Home
+      </Link>
     </div>
   );
 };
